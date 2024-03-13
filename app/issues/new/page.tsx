382cdd7d10a2352f75c340bcee7f75804f56aff2
Daniel Yadghar <Daniel072.dy@gmail.com>
2024-03-13 08:39:41 +0330

    Refactor : Move onSubmit function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -30,6 +30,17 @@ const NewIssuePage = () => {
 
   const router = useRouter();
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/");
+    } catch (error) {
+      setSubmitting(false);
+      setError("Unexpected Error Occured");
+    }
+  });
+
   return (
     <div className="max-w-xl">
       {error && (
@@ -37,19 +48,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className=" space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/");
-          } catch (error) {
-            setSubmitting(false);
-            setError("Unexpected Error Occured");
-          }
-        })}
-      >
+      <form className=" space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title..." {...register("title")} />
         </TextField.Root>
