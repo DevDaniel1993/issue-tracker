24dd44abe93c52ecef088158c85d65c1989a59e5
Daniel Yadghar <Daniel072.dy@gmail.com>
2024-03-16 10:55:45 +0330

    Temperary fixed SimleMDE issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -6,15 +6,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
-const NewIssuePage = async () => {
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+const NewIssuePage = () => {
   const [error, setError] = useState<string>();
   const [isSubmitting, setSubmitting] = useState<boolean>(false);
 
@@ -55,9 +59,7 @@ const NewIssuePage = async () => {
         <Controller
           name="description"
           control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
-          )}
+          render={({ field }) => <SimpleMDE key={1} {...field} />}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
