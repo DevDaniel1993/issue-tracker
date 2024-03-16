9853d23715612b741a6930d8c3de5e879baa9e0c
Daniel Yadghar <Daniel072.dy@gmail.com>
2024-03-16 19:50:04 +0330

    Build an API for updating issues

---
+++ app/api/issues/[id]/route.tsx
@@ -0,0 +1,33 @@
+import { issueSchema } from "@/app/validationSchemas";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../prisma/client";
+
+interface Props {
+  params: { id: string };
+}
+
+export const PATCH = async (
+  request: NextRequest,
+  { params: { id } }: Props
+) => {
+  const body = await request.json();
+
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: parseInt(id) },
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(updatedIssue);
+};
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ErrorMessage, Spinner } from "@/app/components";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from "@radix-ui/themes";
@@ -17,7 +17,7 @@ interface Props {
   issue?: Issue;
 }
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
@@ -33,7 +33,7 @@ const IssueForm = ({ issue }: Props) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
 
   const router = useRouter();
